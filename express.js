309860c3aac668af3eb0fc1c5521335162030f03
qassab <qassab1337@gmail.com>
2024-06-17 23:57:29 +0300

    prod15

--- express.js
+++ express.js
@@ -13,7 +13,7 @@ router.use(function (req,res,next) {
 
 
 router.get('/', function(req,res){
-    res.send('ахуенний відончік!! це з вікна чи з вашого двора?)');
+    res.send('тут мали би бути котики');
 });
 
 app.use('/', router);
