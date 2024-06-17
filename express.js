aa233b4c04c64225b141ef4b3914d81cff67e2e6
qassab <qassab1337@gmail.com>
2024-06-17 15:30:00 +0300

    prod10

--- express.js
+++ express.js
@@ -13,7 +13,7 @@ router.use(function (req,res,next) {
 
 
 router.get('/', function(req,res){
-    res.send('Вы шо ахуели без меня бухать?');
+    res.send('Молчиш - в пезде торчиш');
 });
 
 app.use('/', router);
