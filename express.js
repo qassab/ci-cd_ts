f69dc99c97cdcbb2751b4670844e3636d51c74c7
qassab <qassab1337@gmail.com>
2024-06-17 03:37:12 +0300

    prod8

--- express.js
+++ express.js
@@ -3,7 +3,7 @@ const app = express();
 const router = express.Router(); 
 
 
-const port = 80;
+const port = 8080;
 
 // выводим в консоль HTTP METHOD при каждом запросе
 router.use(function (req,res,next) {
